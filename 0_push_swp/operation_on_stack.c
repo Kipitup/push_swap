8982ff7a94d6b2747eb8cfd69959f021bbb3f825
Alexandre MARTINOD <amartino@e2r13p9.42.fr>
2020-01-27 19:01:43 +0100

    first steps of changing the direction of algo V2

--- 0_push_swp/operation_on_stack.c
+++ 0_push_swp/operation_on_stack.c
@@ -6,7 +6,7 @@
 /*   By: fkante <fkante@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 18:34:04 by fkante            #+#    #+#             */
-/*   Updated: 2020/01/23 21:10:29 by amartino         ###   ########.fr       */
+/*   Updated: 2020/01/27 18:53:17 by amartino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,28 +66,24 @@ void	pa_highest(t_stack *s, size_t limit)
 
 void	pb_under_pivot(t_stack *s, int32_t pivot, size_t limit)
 {
-	size_t	i;
 	size_t	last;
 
-	i = limit;
-	while (i > 0)
+	while (limit > 0)
 	{
 		last = s->size_a - 1;
-		s->a[last] <= pivot ? pb(s) : ra(s);
-		i--;
+		s->a[last] < pivot ? pb(s) : ra(s);
+		limit--;
 	}
 }
 
 void	pa_above_pivot(t_stack *s, int32_t pivot, size_t limit)
 {
-	size_t	i;
 	size_t	last;
 
-	i = limit;
-	while (i > 0)
+	while (limit > 0)
 	{
 		last = s->size_b - 1;
 		s->b[last] >= pivot ? pa(s) : rb(s);
-		i--;
+		limit--;
 	}
 }
